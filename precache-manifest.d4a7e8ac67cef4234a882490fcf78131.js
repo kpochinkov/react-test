302ec69e718994802bc37f887eb842b34aa5aab9
Konstantin Pochinkov <Konstantin.p@solaredge.com>
2019-07-19 14:36:04 +0300

    Updates

---
+++ precache-manifest.d4a7e8ac67cef4234a882490fcf78131.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "709fe46014f2a99d7f8665f551439366",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "0b91308aa905730d70a1",
+    "url": "/react-test/static/css/main.251767c1.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-test/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "0b91308aa905730d70a1",
+    "url": "/react-test/static/js/main.3553b9ec.chunk.js"
+  },
+  {
+    "revision": "91e87af5d0bfc2a79f8e",
+    "url": "/react-test/static/js/runtime~main.b9ad4e01.js"
+  }
+]);
